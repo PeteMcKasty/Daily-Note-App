403eb8d75eb80e7f3f15d5bbeb532d82b810d802
Peter McKasty <pmck25@yahoo.com>
2023-08-22 19:32:40 -0400

    created connection to port and middleware

--- server.js
+++ server.js
@@ -2,3 +2,10 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+app.use(express.static('public'));
