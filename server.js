efc7defba9457c32c3f4bd3116666c7a988ca5a9
Peter McKasty <pmck25@yahoo.com>
2023-08-22 19:42:43 -0400

    added get routes to obtain note data

--- server.js
+++ server.js
@@ -9,3 +9,17 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.json());
 app.use(express.static('public'));
+
+// Routes
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+  });
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  });
+  
+  app.get('/api/notes', (req, res) => {
+    const notesData = JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
+    res.json(notesData);
+  });
